cf5f3b288925149bba26ff67a845a2844713f1cb
BrandonDeoram <brandondeoram8931@gmail.com>
2024-07-09 18:38:26 -0400

    Initial Commit

---
+++ src/main/java/com/demo/MixSplit/MixSplitApplication.java
@@ -0,0 +1,13 @@
+package com.demo.MixSplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixSplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixSplitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/MixSplit/MixSplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.MixSplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixSplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
